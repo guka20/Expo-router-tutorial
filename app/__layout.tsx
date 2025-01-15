060d57844df9e6e719e438e564ca63d5091e8e5b
guka20 <gukadavitadze23@gmail.com>
2025-01-15 23:46:36 +0400

    Setup

---
+++ app/__layout.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+const Main = () => {
+  return (
+    <Text>Hello</Text>
+  )
+}
+
+export default Main
---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import {Text} from 'react-native'
+
+const HomePage = () =>{
+    return(<Text>Hello World</Text>)
+}
+export default HomePage
